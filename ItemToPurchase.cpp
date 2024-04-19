ad57e694955a9beaaf8a7f29e4b56091e175bd04
mollieeh <167226305+mollieeh@users.noreply.github.com>
2024-04-19 14:30:48 -0400

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -11,10 +11,26 @@ ItemToPurchase::ItemToPurchase(){
   itemQuantity = 0;
 }
 
+// setters
+void ItemToPurchase::SetName(string name){
+  itemName = name;
+}
+void ItemToPurchase::SetPrice(int price){
+  itemPrice = price;
+}
 void ItemToPurchase::SetQuantity(int qty){
   itemQuantity = qty;
 }
 
+// getters
+string ItemToPurchase::GetName(){
+  return itemName;
+}
+int ItemToPurchase::GetPrice(){
+  return ItemPrice;
+}
 int ItemToPurchase::GetQuantity(){
   return itemQuantity;
 }
+
+
