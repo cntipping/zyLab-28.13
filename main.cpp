83ea3de391f32875d3fdc417a710910a06cd3924
mollieeh <167226305+mollieeh@users.noreply.github.com>
2024-04-22 14:47:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,38 @@ using namespace std;
 
 int main(){
 
+  string item1, item2;
+  int price, quantity, total;
+
+  
+  cout << "Item 1" << endl << "Enter the item name:" << endl;
+  cin >> item1;
+  item1 = ItemToPurchase(item1);
+  cout << "Enter the item price:" << endl;
+  cin >> price;
+  item1.SetPrice(price);
+  cout << "Enter the item quantity:" << endl;
+  cin >> quantity;
+  item1.SetQuantity(quantity);
+
+  cin.ignore();
+  cout << "Item 2" << endl << "Enter the item name:" << endl;
+  cin >> item2;
+  item2 = ItemToPurchase(item2);
+  cout << "Enter the item price:" << endl;
+  cin >> price;
+  item2.SetPrice(price);
+  cout << "Enter the item quantity:" << endl;
+  cin >> quantity;
+  item2.SetQuantity(quantity);
+
+  total = (item1.GetQuantity() * item1.GetPrice()) + (item2.GetQuantity() * item2.GetPrice());
+
+  cout << "TOTAL COST" << endl;
+  cout << item1.GetName() << " " << item1.GetQuantity() << " @ " << item1.GetPrice() << endl;
+  cout << item2.GetName() << " " << item2.GetQuantity() << " @ " << item2.GetPrice() << endl;
+
+  cout << endl << "Total: $" << total << endl;
 
   return 0;
 }
