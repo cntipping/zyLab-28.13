cfb3c0a25a7e60962a89ef2e5fefd5f1fc73b772
mollieeh <167226305+mollieeh@users.noreply.github.com>
2024-04-22 22:22:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// https://github.com/cntipping/zyLab-28.13
+// Teammates: Cecilia Tipping, Mollie Hamman
+
 #include <iostream>
 using namespace std;
 
@@ -5,40 +8,49 @@ using namespace std;
 // update from user-1
 // update from user-2
 
-int main(){
+int main()
+{
+    string name;
+    int price, quantity;
+    int total, item1Tot, item2Tot;
 
-  string item1, item2;
-  int price, quantity, total;
+    cout << "Item 1" << endl
+         << "Enter the item name:" << endl;
+    getline(cin, name);
+    cout << "Enter the item price:" << endl;
+    cin >> price;
+    cout << "Enter the item quantity:" << endl;
+    cin >> quantity;
+    ItemToPurchase item1(name, price, quantity);
+    cout << endl;
 
-  
-  cout << "Item 1" << endl << "Enter the item name:" << endl;
-  cin >> item1;
-  item1 = ItemToPurchase(item1);
-  cout << "Enter the item price:" << endl;
-  cin >> price;
-  item1.SetPrice(price);
-  cout << "Enter the item quantity:" << endl;
-  cin >> quantity;
-  item1.SetQuantity(quantity);
+    cin.ignore();
+    cout << "Item 2" << endl
+         << "Enter the item name:" << endl;
+    getline(cin, name);
+    cout << "Enter the item price:" << endl;
+    cin >> price;
+    cout << "Enter the item quantity:" << endl;
+    cin >> quantity;
+    ItemToPurchase item2(name, price, quantity);
+    cout << endl;
 
-  cin.ignore();
-  cout << "Item 2" << endl << "Enter the item name:" << endl;
-  cin >> item2;
-  item2 = ItemToPurchase(item2);
-  cout << "Enter the item price:" << endl;
-  cin >> price;
-  item2.SetPrice(price);
-  cout << "Enter the item quantity:" << endl;
-  cin >> quantity;
-  item2.SetQuantity(quantity);
+    item1Tot = item1.GetQuantity() * item1.GetPrice();
+    item2Tot = item2.GetQuantity() * item2.GetPrice();
+    total = item1Tot + item2Tot;
 
-  total = (item1.GetQuantity() * item1.GetPrice()) + (item2.GetQuantity() * item2.GetPrice());
+    cout << "TOTAL COST" << endl;
+    cout << item1.GetName() << " "
+         << item1.GetQuantity() << " @ $"
+         << item1.GetPrice() << " = $"
+         << item1Tot << endl;
+    cout << item2.GetName() << " "
+         << item2.GetQuantity() << " @ $"
+         << item2.GetPrice() << " = $"
+         << item2Tot << endl;
 
-  cout << "TOTAL COST" << endl;
-  cout << item1.GetName() << " " << item1.GetQuantity() << " @ " << item1.GetPrice() << endl;
-  cout << item2.GetName() << " " << item2.GetQuantity() << " @ " << item2.GetPrice() << endl;
+    cout << endl
+         << "Total: $" << total << endl;
 
-  cout << endl << "Total: $" << total << endl;
-
-  return 0;
+    return 0;
 }
